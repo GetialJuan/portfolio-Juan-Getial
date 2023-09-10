1e8e76ffd78175f17f3ab351d379b4446e5930ba
GetialJuan <getial.juan@correounivalle.edu.co>
2023-09-10 15:09:33 -0500

    Add Task 2 - 3D Model with Animation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,10 +1,16 @@
 import Geometries from "./components/Geometries/Geometries";
+import Cat from "./components/World/Cat";
 
 
 const Experience = ({ title, info }) => {
     return (
         <>
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[10, 10, 5]} intensity={2} />
+
             <Geometries />
+
+            <Cat />
         </>
     );
 }
---
+++ src/components/World/Cat.jsx
@@ -0,0 +1,31 @@
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+
+const Cat = () => {
+    const catRef = useRef();
+    const catModel = useGLTF("/assets/models/cat/scene.gltf");
+    console.log("catModel")
+    console.log(catModel)
+    const {animations} = catModel;
+
+    const {actions} = useAnimations(animations, catRef);
+    console.log("actions mio")
+    console.log(actions);
+
+    useEffect(() => {
+        const action = actions["show_lc_001"]
+        console.log('action mio')
+        console.log(action)
+        action.play()
+    }, [])
+
+    return (
+        //[-3.6, 0.9, 0]
+        <mesh ref={catRef} position={[2, -1, 0]} rotation-y={-Math.PI * 0.1} rotation-x={-Math.PI * -0.05} scale={1}>
+            <primitive object={catModel.scene}/>
+        </mesh>
+    )
+}
+
+export default Cat;
+useGLTF.preload("/assets/models/cat/scene.gltf");
